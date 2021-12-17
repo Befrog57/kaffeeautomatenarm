509a64c5f811f2bcf3ccec61b260fa085d92ce10
fschmidt <fschmidt@uni-wuppertal.de>
2021-12-17 12:13:27 +0100

    initial commit

---
+++ arduino.ino
@@ -0,0 +1,27 @@
+String emfangen;
+
+void setup()
+{
+  Serial.begin(9600);  //Boudrate syn mit UART
+  pinMode(A0, OUTPUT);
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    emfangen=Serial.readString();
+    //Löscht Leerzeichen
+    emfangen.trim();
+    if (emfangen == "schnell") {
+        Serial.println("Schnell");
+        analogWrite (A0, 255);
+    } else if (emfangen == "mittel") {
+        Serial.println("Mittel");
+        analogWrite (A0, 190);
+    } else if (emfangen == "langsam") {
+        Serial.println("Langsam");
+        analogWrite (A0, 60);
+    } else if (emfangen == "STOP") {
+        Serial.println("Stop");
+    }
+  }
+}

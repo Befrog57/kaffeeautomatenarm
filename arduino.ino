0450ed7e6c9f7b1858b8221507334dc7859fa549
Fabian Waescher <68521979+DjFabFab@users.noreply.github.com>
2022-01-21 11:45:28 +0100

    Update arduino.ino
    Rewrite code

--- arduino.ino
+++ arduino.ino
@@ -1,28 +1,28 @@
-String emfangen;
-
+//#include <Servo.h>
+//Servo servo;
+String empfangen;
 void setup()
 {
-  Serial.begin(9600);  //Boudrate syn mit UART
   pinMode(A0, OUTPUT);
+  Serial.begin(9600);
 }
 
-void loop() {
-  if (Serial.available() > 0) {
-    emfangen=Serial.readString();
-    //Löscht Leerzeichen
-    emfangen.trim();
-    if (emfangen == "schnell") {
-        Serial.println("Schnell");
-        analogWrite (A0, 255);
-    } else if (emfangen == "mittel") {
-        Serial.println("Mittel");
-        analogWrite (A0, 190);
-    } else if (emfangen == "langsam") {
-        Serial.println("Langsam");
-        analogWrite (A0, 60);
-    } else if (emfangen == "STOP") {
-        Serial.println("STOP");
-        analogWrite (A0, 0);
-    }
+void loop()
+{
+  if(Serial.available() > 0)
+  {
+    empfangen = Serial.readString();
+    empfangen.trim();
+    
+    if (empfangen == "schnell")
+      analogWrite(A0, 255);
+    else if (empfangen == "mittel")
+      analogWrite(A0, 190);
+    else if (empfangen == "langsam")
+      analogWrite(A0, 60);
+    else if (empfangen == "STOP")
+      analogWrite(A0, 0);
+      
+    Serial.println(empfangen);  
   }
 }

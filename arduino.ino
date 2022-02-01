332ad036c7858ff795bb565e143ef730daef4c09
Fabian Waescher <68521979+DjFabFab@users.noreply.github.com>
2022-02-01 09:07:10 +0100

    servo/pwm integration (#4)
    servo/pwm integration

--- arduino.ino
+++ arduino.ino
@@ -1,28 +1,39 @@
-//#include <Servo.h>
-//Servo servo;
+#include <Servo.h>
+
 String empfangen;
-void setup()
-{
-  pinMode(A0, OUTPUT);
+int dly=5;
+Servo myservo;  // create servo object to control a servo
+int pos = 0;    // variable to store the servo position
+boolean stop=true;
+ 
+void setup() {
+  myservo.attach(5);  // attaches the servo on pin 5 to the servo object
   Serial.begin(9600);
 }
-
-void loop()
-{
+ 
+void loop() {
   if(Serial.available() > 0)
   {
     empfangen = Serial.readString();
     empfangen.trim();
-    
+    stop=false;
     if (empfangen == "schnell")
-      analogWrite(A0, 255);
+      dly= 0;
     else if (empfangen == "mittel")
-      analogWrite(A0, 190);
+      dly= 10;
     else if (empfangen == "langsam")
-      analogWrite(A0, 60);
+      dly=20;
     else if (empfangen == "STOP")
-      analogWrite(A0, 0);
-      
+      stop=true;
     Serial.println(empfangen);  
+  }
+  else
+  {
+    dly=5;
+  }
+  for (pos = 0; pos < 360 && stop==false; pos += 1) { // goes from 0 degrees to 360 degrees
+    // in steps of 1 degree
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(dly);
   }
 }

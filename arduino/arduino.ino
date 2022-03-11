5dc924633a202c7d0d2097b279a890ef6e6e1db1
fschmidt <fschmidt@uni-wuppertal.de>
2022-03-11 10:17:13 +0100

    Fix: for loop

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,7 +1,7 @@
 #include <Servo.h>
 #include <Wire.h>
 
-String empfangen;
+bool empfangen;
 int dly=50;
 Servo myservo;  // create servo object to control a servo
 int pos = 0;    // variable to store the servo position
@@ -9,22 +9,29 @@ boolean stop=false;
 
 void setup() {
   Wire.begin(4);
-  Wire.onReceive(starteWasser);
   myservo.attach(2);  // attaches the servo on pin 5 to the servo object
   Serial.begin(9600);
-  myservo.write(200);
+  myservo.write(pos);
 }
 
 void loop() {
   delay(100);
+  Wire.onReceive(starteWasser);
+  for (pos = 0; pos < 155 && stop==false; pos += 1) { // goes from 0 degrees to 360 degrees
+    Serial.println(pos);
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(dly);
+  }
+    for (pos = 155; pos > 0 && stop==false; pos -= 1) { // goes from 0 degrees to 360 degrees
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(dly);
+  }
 }
 
 void starteWasser () {
   if(Wire.available() > 0)
   {
     empfangen = Wire.read();
-    empfangen.trim();
-    //stop=false;
     if (empfangen == true) {
       stop=false;
       dly = 30;
@@ -32,14 +39,5 @@ void starteWasser () {
     else if (empfangen == false)
       stop = true;
     Serial.println(empfangen);
-  }
-
-  for (pos = 0; pos < 155 && stop==false; pos += 1) { // goes from 0 degrees to 360 degrees
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(dly);
-  }
-    for (pos = 155; pos > 0 && stop==false; pos -= 1) { // goes from 0 degrees to 360 degrees
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(dly);
   }
 }
